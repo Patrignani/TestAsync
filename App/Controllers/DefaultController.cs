03947a7549d9aa03bd0f06fae32276cb609b717e
andersonpatrignani <anderson.patrignani@gmail.com>
2019-12-08 17:58:16 -0300

    no message

---
+++ App/Controllers/DefaultController.cs
@@ -0,0 +1,88 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Dapper;
+using System.Threading.Tasks;
+using Microsoft.Data.SqlClient;
+using System;
+
+namespace TestAsync.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class DefaultController : ControllerBase
+    {
+        private readonly string _connectionString;
+
+        public DefaultController(IConfiguration configuration)
+        {
+            _connectionString = configuration.GetConnectionString("TestAsync");
+        }
+
+        [HttpGet]
+        [Route("Async")]
+        public async Task<ActionResult<string>> Async()
+        {
+            using (var connection = new SqlConnection(_connectionString))
+            {
+                connection.Open();
+                var sequence = Guid.NewGuid();
+                var count = connection.ExecuteScalar<int>("SELECT COUNT(*) FROM Logs");
+                var insert = "INSERT INTO Logs(Message, DataExec, Sequence, Async) VALUES(@Message, @DataExec, @Sequence, 1)";
+                connection.Execute(insert, new
+                {
+                    Message = $"Método Assíncrono iniciado. Contagem {count}",
+                    DataExec = DateTime.UtcNow.ToString("dd/MM/yyyy hh:mm:ss"),
+                    Sequence = sequence
+                });
+
+                await Task.Delay(10);
+                await Task.Delay(48);
+                await Task.Delay(61);
+                await Task.Delay(120);
+                await Task.Delay(4);
+
+                connection.Execute(insert, new
+                {
+                    Message = $"Método Assíncrona Finalizado. Contagem {count}",
+                    DataExec = DateTime.UtcNow.ToString("dd/MM/yyyy hh:mm:ss"),
+                    Sequence = sequence
+                });
+            }
+            return "Ok";
+        }
+
+        [HttpGet]
+        [Route("Sync")]
+        public ActionResult<string> Sync()
+        {
+            using (var connection = new SqlConnection(_connectionString))
+            {
+                connection.Open();
+                var sequence = Guid.NewGuid();
+                var count = connection.ExecuteScalar<int>("SELECT COUNT(*) FROM Logs");
+                var insert = "INSERT INTO Logs(Message, DataExec, Sequence, Async) VALUES(@Message, @DataExec, @Sequence, 0)";
+                connection.Execute(insert, new
+                {
+                    Message = $"Método Síncrona iniciado. Contagem {count}",
+                    DataExec = DateTime.UtcNow.ToString("dd/MM/yyyy hh:mm:ss"),
+                    Sequence = sequence
+                });
+
+                Task.Delay(10).Wait();
+                Task.Delay(48).Wait();
+                Task.Delay(61).Wait();
+                Task.Delay(120).Wait();
+                Task.Delay(4).Wait();
+
+                connection.Execute(insert, new
+                {
+                    Message = $"Método Síncrona Finalizado. Contagem {count}",
+                    DataExec = DateTime.UtcNow.ToString("dd/MM/yyyy hh:mm:ss"),
+                    Sequence = sequence
+                });
+            }
+            return "Ok";
+        }
+
+    }
+}
---
+++ Console/Program.cs
@@ -0,0 +1,45 @@
+﻿using RestSharp;
+using System;
+using System.Collections.Generic;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace AttackAsync
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            var task = new List<Task>();
+            for (var a = 0; a < 100; a++)
+            {
+                //task.Add(testAsync(a));
+                task.Add(test(a));
+            }
+
+            await Task.WhenAll(task);
+        }
+
+        public static async Task test(int a)
+        {
+
+            Console.WriteLine(a);
+            var client = new RestClient("https://localhost:44398/api/default/sync");
+            var request = new RestRequest(Method.GET);
+            var cancellationTokenSource = new CancellationTokenSource();
+            IRestResponse response = await client.ExecuteTaskAsync(request, cancellationTokenSource.Token);
+            Console.WriteLine(response.Content);
+        }
+
+        public static async Task testAsync(int a)
+        {
+
+            Console.WriteLine(a);
+            var client = new RestClient("https://localhost:44398/api/default/async");
+            var request = new RestRequest(Method.GET);
+            var cancellationTokenSource = new CancellationTokenSource();
+            IRestResponse response = await client.ExecuteTaskAsync(request, cancellationTokenSource.Token);
+            Console.WriteLine(response.Content);
+        }
+    }
+}
